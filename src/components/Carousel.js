e83ba980b5e686f1df7138651047e12ea3a6cdc9
William Galvao <wsg.online@gmail.com>
2021-12-02 17:23:17 -0300

    fixed bootstrap issues

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,28 +5,40 @@ import ages from "../assets/ages.jpg";
 
 export default function Carousel () {
     return (
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div className="carousel-inner">
+  <div className="carousel-inner display-4">
     <div className="carousel-item active">
-      <img src={together} className="d-block w-100" alt="Treinador com seu aluno"/>
+      <img src={together} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <p className="display-1 carousel-title">Treinamento personalizado</p>
+        <p className="carousel-subtitle">Basta de métodos genéricos. Seu corpo: suas necessidades.</p>
+      </div>
     </div>
     <div className="carousel-item">
-      <img src={anywhere} className="d-block w-100" alt="Aluno treinando em casa"/>
+      <img src={anywhere} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <p className="display-1 carousel-title">Onde e quando você puder</p>
+        <p className="carousel-subtitle">Na academia, na sua casa ou no trabalho, eu vou até você.</p>
+      </div>
     </div>
     <div className="carousel-item">
-      <img src={ages} className="d-block w-100" alt="Idosos treinando"/>
+      <img src={ages} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <p className="display-1 carousel-title">Saúde em primeiro lugar</p>
+        <p className="carousel-subtitle">Atividades que irão melhorar a sua vida, não importa a idade.</p>
+      </div>
     </div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
