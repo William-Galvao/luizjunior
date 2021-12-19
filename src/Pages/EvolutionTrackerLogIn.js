7feced802955faddb17fd8310f1ee22546fdb87f
William Galvao <wsg.online@gmail.com>
2021-12-19 09:58:25 -0300

    google login button created

--- src/Pages/EvolutionTrackerLogIn.js
+++ src/Pages/EvolutionTrackerLogIn.js
@@ -1,3 +1,18 @@
+import googleIcon from "../assets/google-icon.svg";
+
 export default function EvolutionTrackerLogIn() {
-  return <div></div>;
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "75vh" }}
+    >
+      <button
+        type="button"
+        className="btn btn-danger btn-lg d-flex justify-content-center align-items-center"
+      >
+        <img src={googleIcon} alt="google icon" className="pe-3" />
+        Entre com sua conta Google
+      </button>
+    </div>
+  );
 }
